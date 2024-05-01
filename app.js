1536d0aa5aadd267c8aeb35dffe7d433e1800925
DimasPramantya <igkdimas@gmail.com>
2024-05-01 22:10:04 +0700

    INITIAL COMMIT

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req,res,next)=>{
+  res.status(200).json({
+    message: "Hello World"
+  })
+})
+
+app.listen(8080, ()=>{
+  console.log("Server is listening on PORT 8080");
+})
